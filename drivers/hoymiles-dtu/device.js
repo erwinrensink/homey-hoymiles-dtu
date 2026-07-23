cfd6b16bcd4fb817ce91e09d994f73e32d05bbee
Erwin Rensink <erwinrensink@gmail.com>
2026-07-23 19:40:34 +0200

    Refactor Hoymiles parser architecture
    - Added HoymilesParser class
    - Read complete inverter register block
    - Removed old register mapping
    - Integrated parser into HoymilesClient
    - Simplified device polling
    - Stable Modbus TCP communication

--- drivers/hoymiles-dtu/device.js
+++ drivers/hoymiles-dtu/device.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Homey = require('homey');
+const HoymilesClient = require('../../lib/hoymiles/client');
 
 module.exports = class HoymilesDTUDevice extends Homey.Device {
 
@@ -8,14 +9,54 @@ module.exports = class HoymilesDTUDevice extends Homey.Device {
    * onInit is called when the device is initialized.
    */
   async onInit() {
+
     this.log('Hoymiles DTU Device initialized');
+
+    const settings = this.getSettings();
+
+    const host = settings.ip;
+    const port = Number(settings.port);
+    const unitId = Number(settings.unitId);
+
+    this.log(`Settings:
+    IP      : ${host}
+    Port    : ${port}
+    Unit ID : ${unitId}`);
+
+    this.log(`Connecting to ${host}:${port}`);
+    this.client = new HoymilesClient(host, port, unitId);
+
+    try {
+      await this.client.connect();
+      this.log('Connected to Hoymiles DTU');
+      // await this.client.scanMicroinverters();
+      await this.updateValues();  
+    } catch (err) {
+      this.error(err);
+    }
+
+    this.pollTimer = this.homey.setInterval(async () => {
+      try {
+        await this.updateValues();
+      } catch (err) {
+        this.error(err);
+      }
+    }, 10000);
   }
 
   /**
    * onAdded is called when the user adds the device, called just after pairing.
    */
   async onAdded() {
-    this.log('MyDevice has been added');
+
+    this.log('Device added');
+
+    await this.setSettings({
+      ip: '192.168.2.73',
+      port: 502,
+      unitId: 1,
+    });
+
   }
 
   /**
@@ -43,7 +84,19 @@ module.exports = class HoymilesDTUDevice extends Homey.Device {
    * onDeleted is called when the user deleted the device.
    */
   async onDeleted() {
+    if (this.pollTimer) {
+      this.homey.clearInterval(this.pollTimer);
+    }
+    await this.client.disconnect();
     this.log('MyDevice has been deleted');
   }
 
+  async updateValues() {
+    const data = await this.client.readRealtimeData();
+    this.log('Realtime data:', JSON.stringify(data));
+    await this.setCapabilityValue(
+      'measure_power',
+      data.power
+    );
+  }  
 };
--- drivers/hoymiles-dtu/driver.js
+++ drivers/hoymiles-dtu/driver.js
@@ -4,26 +4,26 @@ const Homey = require('homey');
 
 module.exports = class HoymilesDTUDriver extends Homey.Driver {
 
-  /**
-   * onInit is called when the driver is initialized.
-   */
   async onInit() {
     this.log('Hoymiles DTU Driver initialized');
   }
 
-  /**
-   * onPairListDevices is called when a user is adding a device
-   * and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
+  async onPair(session) {
 
-    return [{
-      name: 'Hoymiles DTU',
-      data: {
-        id: 'hoymiles-dtu',
-      },
-    }];
+    this.log('Pair session started');
+
+    session.setHandler('list_devices', async () => {
+
+      this.log('list_devices called');
+
+      return [{
+        name: 'Hoymiles DTU',
+        data: {
+          id: 'hoymiles-dtu',
+        },
+      }];
+
+    });
 
   }
 
